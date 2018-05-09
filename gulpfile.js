6900c91b3abb3bb1ccf3ec29fb31830d9c3193d0
Dan O. Williams <daniel.williams@gsa.gov>
2018-05-09 16:23:38 -0700

    Prepare init

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var pkg           = require('./package.json');
 var autoprefixer  = require('autoprefixer');
 var cssnano       = require('cssnano');
 var del           = require('del');
@@ -7,6 +8,7 @@ var movecss       = require('css-mqpacker');
 var path          = require('path');
 var postcss       = require('gulp-postcss');
 var rename        = require('gulp-rename');
+var replace       = require('gulp-replace');
 var sass          = require('gulp-sass');
 var size          = require('gulp-size');
 var uncss         = require('postcss-uncss');
@@ -24,7 +26,7 @@ const USWDS_SRC         = 'node_modules/uswds/dist';
 // Project Sass source directory
 const PROJECT_SASS_SRC  = '_sass';
 
-// Asset destination
+// Asset (images, fonts) destination
 const ASSETS_DEST       = 'assets/uswds';
 
 // CSS destination
@@ -36,12 +38,20 @@ const BUILD_DEST        = '_site';
 // Include destination
 const INC_DEST          = '_includes';
 
+// Primary stylesheet name (exclude .css)
+const STYLESHEET_BASE   = 'styles';
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // BUILD USWDS STYLES
 
 gulp.task('copy-uswds-assets', () => {
-  return gulp.src(`${USWDS_SRC}/@(js|fonts|img)/**/**`)
+  return gulp.src(`${USWDS_SRC}/@(fonts|img)/**/**`)
   .pipe(gulp.dest(`${ASSETS_DEST}`));
+});
+
+gulp.task('copy-uswds-settings', () => {
+  return gulp.src(`${USWDS_SRC}/stylesheets/project/**/**`)
+  .pipe(gulp.dest(`${PROJECT_SASS_SRC}`));
 });
 
 gulp.task('clean-css', function () {
@@ -62,11 +72,16 @@ gulp.task('build-sass', ['clean-css'], function (done) {
     .pipe(sass({
         includePaths: [
           `${PROJECT_SASS_SRC}`,
-          `${USWDS_SRC}/stylesheets`,
+          `${USWDS_SRC}/scss`,
+          `${USWDS_SRC}/scss/packages`,
         ]
       }))
     .pipe(gulp.dest(`${CSS_DEST}`))
     .pipe(postcss(plugins))
+    .pipe(replace(
+      /\buswds @version\b/g,
+      'uswds v' + pkg.version
+    ))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(`${CSS_DEST}`))
     .pipe(size())
@@ -83,9 +98,9 @@ gulp.task('build-app', ['build-sass'], function() {
     }),
     cssnano()
   ];
-  return gulp.src(`${CSS_DEST}/styles.min.css`)
+  return gulp.src(`${CSS_DEST}/${STYLESHEET_BASE}.min.css`)
     .pipe(postcss(plugins))
-    .pipe(rename('styles.app.css'))
+    .pipe(rename(`${STYLESHEET_BASE}.app.css`))
     .pipe(gulp.dest(`${INC_DEST}`))
     .pipe(gulp.dest(`${CSS_DEST}`))
     .pipe(size())
@@ -101,6 +116,9 @@ gulp.task('watch', ['build-sass'], function (){
   gulp.watch(`${USWDS_SRC}/scss/*.scss`, ['build-sass']);
 });
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Initialize project
+gulp.task('uswds-init', ['copy-uswds-assets', 'copy-uswds-settings']);
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Set watch as default task
