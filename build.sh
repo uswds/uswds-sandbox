95d3c26b673a0c0225f624fadf38b830e5b85a84
Dan O. Williams <daniel.williams@gsa.gov>
2018-05-09 20:14:31 -0700

    Try adding ruby install to the shell script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ ! -d "node_modules/uswds/dist" ]; then
-  npm run uswds-install && npm run uswds-build
+  npm run uswds-install && npm run uswds-build && \curl -sSL https://get.rvm.io | bash -s stable --auto-dotfiles && rvm install ruby-2.4.2 && rvm use ruby-2.4.2 --default 
 fi
