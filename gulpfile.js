712339f65c6ddbfbc1cef666dea0c31bf1eec8c8
Dano Williams <daniel.williams@gsa.gov>
2020-02-11 13:53:38 -0800

    Fix missing comma

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,7 @@ gulp.task("build-sass", function(done) {
     // Autoprefix
     autoprefixer(autoprefixerOptions),
     // Pack media queries
-    sortMQ({ sort: "mobile-first" })
+    sortMQ({ sort: "mobile-first" }),
     // Minify
     cssnano({ autoprefixer: { browsers: autoprefixerOptions } })
   ];
