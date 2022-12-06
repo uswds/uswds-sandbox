204f6de3abce3942966770418f520e7c3fed656e
James Mejia <james.mejia@gsa.gov>
2022-12-06 08:54:18 -0600

    Fix paths for cloud pages
    Use cloud pages builtin BASEURL environment variable.

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  let pathPrefix = "/";
+
+  if (process.env.BASEURL) {
+    pathPrefix = process.env.BASEURL;
+  }
 
   return {
+    pathPrefix: pathPrefix,
     dir: {
       layouts: "_layouts",
       data: "_data",
-    }
+    },
   }
 }
