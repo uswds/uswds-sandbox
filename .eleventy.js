00214c536a684a7b857fd9abc8d56d7213534aa3
James Mejia <james.mejia@gsa.gov>
2022-12-08 11:25:02 -0600

    Watch for SASS changes
    - Watch SASS directory in config
    - Set throttle to wait for USWDS to compile

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addWatchTarget("./_sass");
+
+  // Wait for SASS to compile, set to 2s because our compile task is 1.7s
+  eleventyConfig.setWatchThrottleWaitTime(2000);
+
   let pathPrefix = "/";
 
   if (process.env.BASEURL) {
