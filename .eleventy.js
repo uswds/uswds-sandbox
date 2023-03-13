8a416f98ff272be42d81292c5f4e051599f58aaf
James Mejia <james.mejia@gsa.gov>
2023-03-13 08:33:37 -0500

    Add slinkity react
    A lot of peer dependency issues to get to the right setup. Slinkity documentation isn't up-to-date.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
 // TODO: After Canary 19:
 // const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const slinkity = require("slinkity");
+const react = require("@slinkity/react");
 
 module.exports = function (eleventyConfig) {
   // TODO: After Canary 19:
   // eleventyConfig.addPlugin(EleventyHtmlBasePlugin);  eleventyConfig.addPassthroughCopy("src/assets");
+
+  eleventyConfig.addPlugin(slinkity.plugin, slinkity.defineConfig({
+    renderers: [react()],
+  }));
+
 	eleventyConfig.setBrowserSyncConfig({
 		files: './_site/assets/css/**/*.css'
 	});
