c73ebe4d36a52b685592b80642d7cc212c61f049
Matt Henry <matt.henry@gsa.gov>
2024-05-01 15:35:58 -0400

    Add webc dependency; test webc page

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,16 @@
 // TODO: After Canary 19:
 // const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+//
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (eleventyConfig) {
   // TODO: After Canary 19:
   // eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(pluginWebc);
   eleventyConfig.addPassthroughCopy("src/assets");
-	eleventyConfig.setBrowserSyncConfig({
-		files: './_site/assets/css/**/*.css'
-	});
+  eleventyConfig.setBrowserSyncConfig({
+    files: "./_site/assets/css/**/*.css",
+  });
 
   let baseurl = "/";
   if (process.env.BASEURL) {
