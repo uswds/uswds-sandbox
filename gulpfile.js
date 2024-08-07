3779ac2f3edb09df48428a475f4860d4a8347e7e
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2024-08-07 14:40:49 -0400

    Add additional scripts to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,15 @@ uswds.settings.version = 2;
 uswds.paths.dist.css = "./assets/css";
 uswds.paths.dist.theme = "./sass";
 
+// Images destination
+uswds.paths.dist.img = "./assets/img";
+
+// Fonts destination
+uswds.paths.dist.fonts = "./assets/fonts";
+
+// Javascript destination
+uswds.paths.dist.js = "./assets/js";
+
 /**
  * Exports
  * Add as many as you need
@@ -23,3 +32,12 @@ uswds.paths.dist.theme = "./sass";
 
 exports.init = uswds.init;
 exports.compile = uswds.compile;
+exports.compileIcons = uswds.compileIcons;
+exports.copyFonts = uswds.copyFonts;
+exports.copyImages = uswds.copyImages;
+exports.copyJS = uswds.copyJS;
+exports.copyAssets = uswds.copyAssets;
+exports.updateUswds = uswds.updateUswds;
+exports.buildSass = uswds.compileSass;
+exports.watchSass = uswds.watch;
+exports.default = uswds.watch;
