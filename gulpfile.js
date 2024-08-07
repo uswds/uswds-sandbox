89ed041d1ec2bad333ca865845dce6ab705f6ada
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2024-08-07 13:01:14 -0400

    Install compile 1.2.0; run init and compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,130 +1,25 @@
-/*
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-========================================
-========================================
-========================================
-----------------------------------------
-USWDS SASS GULPFILE
-----------------------------------------
-*/
+/* gulpfile.js */
 
-const autoprefixer = require("autoprefixer");
-const csso = require("postcss-csso");
-const gulp = require("gulp");
-const pkg = require("./node_modules/uswds/package.json");
-const postcss = require("gulp-postcss");
-const replace = require("gulp-replace");
-const sass = require("gulp-sass");
-const sourcemaps = require("gulp-sourcemaps");
-const uswds = require("./node_modules/uswds-gulp/config/uswds");
+const uswds = require("@uswds/compile");
 
-sass.compiler = require("sass");
+/**
+ * USWDS version
+ */
 
-/*
-----------------------------------------
-PATHS
-----------------------------------------
-- All paths are relative to the
-  project root
-- Don't use a trailing `/` for path
-  names
-----------------------------------------
-*/
+uswds.settings.version = 2;
 
-// Project Sass source directory
-const PROJECT_SASS_SRC = "./_sass";
+/**
+ * Path settings
+ * Set as many as you need
+ */
 
-// Images destination
-const IMG_DEST = "./assets/uswds/img";
+uswds.paths.dist.css = "./assets/css";
+uswds.paths.dist.theme = "./sass";
 
-// Fonts destination
-const FONTS_DEST = "./assets/uswds/fonts";
+/**
+ * Exports
+ * Add as many as you need
+ */
 
-// Javascript destination
-const JS_DEST = "./assets/uswds/js";
-
-// Compiled CSS destination
-const CSS_DEST = "./assets/uswds/css";
-
-// Site CSS destination
-// Like the _site/assets/css directory in Jekyll, if necessary.
-// If using, uncomment line 106
-const SITE_CSS_DEST = "./_site/assets/uswds/css";
-
-/*
-----------------------------------------
-TASKS
-----------------------------------------
-*/
-
-gulp.task("copy-uswds-setup", () => {
-  return gulp
-    .src(`${uswds}/scss/theme/**/**`)
-    .pipe(gulp.dest(`${PROJECT_SASS_SRC}`));
-});
-
-gulp.task("copy-uswds-fonts", () => {
-  return gulp.src(`${uswds}/fonts/**/**`).pipe(gulp.dest(`${FONTS_DEST}`));
-});
-
-gulp.task("copy-uswds-images", () => {
-  return gulp.src(`${uswds}/img/**/**`).pipe(gulp.dest(`${IMG_DEST}`));
-});
-
-gulp.task("copy-uswds-js", () => {
-  return gulp.src(`${uswds}/js/**/**`).pipe(gulp.dest(`${JS_DEST}`));
-});
-
-gulp.task("build-sass", function (done) {
-  var plugins = [
-    // Autoprefix
-    autoprefixer({
-      cascade: false,
-      grid: true,
-    }),
-    // Minify
-    csso({ forceMediaMerge: false }),
-  ];
-  return (
-    gulp
-      .src([`${PROJECT_SASS_SRC}/*.scss`])
-      .pipe(sourcemaps.init({ largeFile: true }))
-      .pipe(
-        sass.sync({
-          includePaths: [
-            `${PROJECT_SASS_SRC}`,
-            `${uswds}/scss`,
-            `${uswds}/scss/packages`,
-          ],
-        })
-      )
-      .pipe(replace(/\buswds @version\b/g, "based on uswds v" + pkg.version))
-      .pipe(postcss(plugins))
-      .pipe(sourcemaps.write("."))
-      // uncomment the next line if necessary for Jekyll to build properly
-      .pipe(gulp.dest(`${SITE_CSS_DEST}`))
-      .pipe(gulp.dest(`${CSS_DEST}`))
-  );
-});
-
-gulp.task(
-  "init",
-  gulp.series(
-    "copy-uswds-setup",
-    "copy-uswds-fonts",
-    "copy-uswds-images",
-    "copy-uswds-js",
-    "build-sass"
-  )
-);
-
-gulp.task("watch-sass", function () {
-  gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series("build-sass"));
-});
-
-gulp.task("watch", gulp.series("build-sass", "watch-sass"));
-
-gulp.task("default", gulp.series("watch"));
+exports.init = uswds.init;
+exports.compile = uswds.compile;
