fc93ed688f8e2ef7bbeabe4d2a27ed11b4c970d7
James Mejia <james.mejia@gsa.gov>
2024-12-31 13:09:56 -0600

    Test: vite commonjs plugin

--- src/assets/project/js/index.js
+++ src/assets/project/js/index.js
@@ -1,9 +1,9 @@
-import usaModal from "@uswds/uswds/js/usa-modal";
+import modal from "@uswds/uswds/js/usa-modal";
 
 (function () {
   document.addEventListener("DOMContentLoaded", () => {
     const myProjectModal = document.querySelectorAll(".usa-modal");
 
-    usaModal.on(myProjectModal);
+    modal.on(myProjectModal);
   });
 })();
