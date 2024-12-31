4fd5eb9640b339df9ea8575dd336ae3627c05749
James Mejia <james.mejia@gsa.gov>
2024-12-31 13:20:11 -0600

    Pass plugin to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+import commonjs from "vite-plugin-commonjs";
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [commonjs()],
   build: {
     rollupOptions: {
       external: ["@uswds/uswds"],
