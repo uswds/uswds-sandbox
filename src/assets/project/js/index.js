d8655b9150c5393230c5a6398e744c9c64b13d4e
mlloydbixal <amanda.lloyd@gsa.gov>
2025-01-09 13:46:59 -0500

    add testing examples

--- src/assets/project/js/index.js
+++ src/assets/project/js/index.js
@@ -3,7 +3,18 @@ import modal from "@uswds/uswds/js/usa-modal";
 (function () {
   document.addEventListener("DOMContentLoaded", () => {
     const myProjectModal = document.querySelectorAll(".usa-modal");
-
     modal.on(myProjectModal);
+
+    // Toggle modal on
+    // modal.toggle("example-modal-3");
+
+    // Toggle modal off
+    // modal.toggle("example-modal-3");
+
+    // Trigger modal show
+    // modal.show("example-modal-3");
+
+    // Trigger modal hide
+    // modal.hide("example-modal-3");
   });
 })();
